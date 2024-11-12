69af20b6770640c0968778e11937d7f3ac6539fd
Mario Roberto <mrgomez.dev@gmail.com>
2024-11-11 23:11:45 -0600

    Parámetros de ruta y consultas en FastAPI

--- curso-fastapi-project/main.py
+++ curso-fastapi-project/main.py
@@ -1,3 +1,5 @@
+import zoneinfo
+from datetime import datetime
 from fastapi import FastAPI
 
 
@@ -7,3 +9,21 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message":  "Hola Mario!"}
+
+
+country_timezone = {
+    "CO": "America/Bogota",
+    "MX": "America/Mexico_City",
+    "AR": "America/Argentina/Buenos_Aires",
+    "BR": "America/Sao_Paulo",
+    "PE": "America/Lima",
+}
+
+
+@app.get("/time/{iso_code}")
+async def time(iso_code: str):
+    iso = iso_code.upper()
+    country_timezone.get(iso)
+    timezone_str = country_timezone.get(iso)
+    tz = zoneinfo.ZoneInfo(timezone_str)
+    return {"time": datetime.now(tz)}
